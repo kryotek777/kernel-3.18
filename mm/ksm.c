ff533ad4cb6618f793eb1fab67caa1bdb4f87c3a
Kryotek <gcda@outlook.it>
2023-01-19 02:39:42 +0100

    ksm missing semicolon

--- mm/ksm.c
+++ mm/ksm.c
@@ -1820,7 +1820,7 @@ static int ksm_fb_notifier_callback(struct notifier_block *p,
 
 static struct notifier_block ksm_fb_notifier = {
 	.notifier_call = ksm_fb_notifier_callback,
-}
+};
 #endif
 #else /* no KSM_KCTL_INTERFACE*/
 static ssize_t ksm_run_change(unsigned long flags)
